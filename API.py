53e116305b8e80d80307467ba80b31c7aaf4f21c
Keshava Rao <keshavarao@Keshavas-MacBook-Air.local>
2025-01-25 07:43:24 +0530

    Initial commit for new tracking

---
+++ API.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, HTTPException, status, Request, Depends
+import uvicorn
+from Backend import Agent
+import CONSTANTS  # type: ignore
+from pydantic import ValidationError
+from typing import Dict, Any
+from collections import Counter
+import logging
+from fastapi.middleware.cors import CORSMiddleware
+from logging.handlers import RotatingFileHandler
+
+app = FastAPI()
+counter_calls = Counter()
+
+
+file_name = CONSTANTS.AWS_FILE
+s3 = CONSTANTS.AWS_S3
+agent = Agent(s3=s3, file_name=file_name)
+
+"""
+ 1. CREATE APIs with Security, Authenticity and Authority.
+ 2. API for Data Security and Authenticity.
+ 3. API for Backups.
+ """
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+handler = RotatingFileHandler('application.log', maxBytes=1000000, backupCount=3)
+logging.basicConfig(handlers=[handler], level=logging.INFO)
+@app.middleware("http")
+async def count_api_calls(request: Request, call_next):
+    response = await call_next(request)
+    counter_calls["totalCalls"] += 1
+    print('Total API Calls: ',counter_calls["totalCalls"])
+    print('Current Session-Calling:',counter_calls["totalCalls"],request.method,':', request.url)
+    logging.info(f"Log from API EndPoint - Current Session-Calling Count: '{counter_calls['totalCalls']}'. Method: '{request.method}'. EndPoint: '{request.url}'")
+    return response
+
+def process_data(item,operation):
+    try:
+        match operation:
+            case 'insert':
+                response = agent.insert_new_data(item)
+            case 'update':
+                response = agent.update_one_data(item)
+            case 'delete':
+                response = agent.delete_one_data(item)
+            case 'get':
+                return agent.get_all_data()
+            case _:
+                raise ValueError("Invalid operation")
+        
+        if response:
+            return {"message": f"PII data {operation}ed successfully","response":response}
+        else:
+            return {"message": f"Failed to {operation} PII data. Reason: {response}"}
+    except ValidationError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+#1. API for CREATE
+@app.post("/pii")
+async def insert_pii_item(item: Dict[str, Any]):
+    return process_data(item, 'insert')
+
+
+@app.patch("/pii")
+async def update_pii_item(item: Dict[str, Any]):
+    return process_data(item, 'update')
+
+
+@app.delete("/pii")
+async def delete_pii_item(item: Dict[str, Any]):
+    return process_data(item, 'delete')
+
+@app.get("/pii")
+async def get_pii_data():
+   return process_data(None, 'get')
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ KMS.py
@@ -0,0 +1,76 @@
+import boto3
+import os
+from dataclasses import dataclass
+import base64
+import pandas as pd
+import CONSTANTS
+from cryptography.fernet import Fernet
+
+
+
+class KMS:
+    def __init__(self):
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        self.kms = boto3.client('kms')
+        self.data_path:str = CONSTANTS.AWS_FILE         
+        
+    def decrypt_my_key(self, key):
+        kms_client = boto3.client('kms')
+
+        # Ensure 'key' is in byte format
+        if isinstance(key, str):
+            try:
+                key = base64.b64decode(key)  # Convert from base64-encoded string to bytes
+            except Exception as e:
+                print("Error decoding base64 key:", e)
+                raise
+
+        try:
+            response = kms_client.decrypt(CiphertextBlob=key)
+            fernet_key = base64.urlsafe_b64encode(response['Plaintext'])
+            self.cipher_suite = Fernet(fernet_key)
+            return self.cipher_suite
+        except kms_client.exceptions.InvalidCiphertextException:
+            # self.create_new_key()
+            print("Decryption failed: Invalid ciphertext")
+            raise
+        except Exception as e:
+            print("General decryption error:", e)
+            raise
+
+    
+    def create_new_key(self):
+        self.data_path:str = self.file_name
+        self.__df = pd.read_excel(self.data_path)
+        self.key = self.generate_secure_key('AES_256')
+        self.__key = self.key['Plaintext']
+        self.storing_key = self.key['CiphertextBlob']
+        self.cipher_suite = Fernet(base64.urlsafe_b64encode(self.__key))
+        return self.cipher_suite,self.__df
+
+    
+    
+    def generate_secure_key(self, key_spec):
+        aws_key = os.getenv('AWS_KEY')
+        assert aws_key != None
+        self.__key_id = aws_key
+        response = self.kms.generate_data_key(
+            KeyId=self.__key_id,
+            KeySpec=key_spec
+        )
+        return response
+    
+
+    def create_new_key(self):
+        self.__df = pd.read_excel(self.data_path)
+        self.key = self.generate_secure_key('AES_256')
+        self.__key = self.key['Plaintext']
+        self.storing_key = self.key['CiphertextBlob']
+        self.cipher_suite = Fernet(base64.urlsafe_b64encode(self.__key))
+        return self.cipher_suite
+
+    def decrypt_data(self,item):
+        return self.cipher_suite.decrypt(item).decode('utf-8')
+   
+    
+  
